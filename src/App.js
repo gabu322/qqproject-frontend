7d83b981f413c76f085070dbbc815354f44a570a
Gabriel DallOrto <gabrieldallorto@hotmail.com>
2023-03-09 18:52:42 -0300

    -m

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+  MDBInput,
+  MDBCol,
+  MDBRow,
+  MDBCheckbox,
+  MDBBtn
+} from 'mdb-react-ui-kit';
+import './App.css'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='testing shadow-5 text-dark'>
+      <img src='/logo.png' className='img-fluid' alt='...' />
+      <form>
+        <MDBInput className='mb-4' type='email' id='form1Example1' label='Email address' />
+
+
+        <MDBRow className='mb-4 flex-column'>
+          <MDBCol>
+          <MDBInput className='mb-4' type='password' id='form1Example2' label='Password' />
+
+          </MDBCol>
+          <MDBCol className='d-flex justify-content-end'>
+            <a href='#!'>Forgot password?</a>
+          </MDBCol>
+        </MDBRow>
+
+        <MDBBtn type='submit' block color='success'>
+          LOG IN
+        </MDBBtn>
+      </form>
+      <hr></hr>
+      <MDBBtn block color='success' tag='a' href='#'>
+          Cadastrar-se
+        </MDBBtn>
+
     </div>
   );
 }
-
-export default App;
