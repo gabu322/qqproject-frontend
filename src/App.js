866d311932ef0755c1dbd851528e03d8d2305bfb
Gabriel DallOrto <gabrieldallorto@hotmail.com>
2023-03-14 13:50:22 -0300

    Login done

--- src/App.js
+++ src/App.js
@@ -1,40 +1,40 @@
 import React from 'react';
 import {
-  MDBInput,
-  MDBCol,
-  MDBRow,
-  MDBCheckbox,
-  MDBBtn
+	MDBInput,
+	MDBCol,
+	MDBRow,
+	MDBCheckbox,
+	MDBBtn
 } from 'mdb-react-ui-kit';
 import './App.css'
 
 export default function App() {
-  return (
-    <div className='testing shadow-5 text-dark'>
-      <img src='/logo.png' className='img-fluid' alt='...' />
-      <form>
-        <MDBInput className='mb-4' type='email' id='form1Example1' label='Email address' />
+	return (
+		<div className='main shadow-5 text-dark'>
+			<img src='/logo.png' className='img-fluid mb-4' alt='...' />
+			<div className='inputs'>
+				<form>
+					<MDBInput className='mb-4' type='email' id='form1Example1' label='Email ou matrícula' />
+					<MDBInput className='mb-4' type='password' id='form1Example2' label='Senha' />
 
+					<MDBRow className='mb-4'>
+						<MDBCol className='d-flex justify-content-center'>
+							<MDBCheckbox id='form1Example3' label='Permanecer conectado' defaultChecked />
+						</MDBCol>
+						<MDBCol className='d-flex justify-content-center'>
+							<a href='#!'>Esqueceu sua senha?</a>
+						</MDBCol>
+					</MDBRow>
 
-        <MDBRow className='mb-4 flex-column'>
-          <MDBCol>
-          <MDBInput className='mb-4' type='password' id='form1Example2' label='Password' />
-
-          </MDBCol>
-          <MDBCol className='d-flex justify-content-end'>
-            <a href='#!'>Forgot password?</a>
-          </MDBCol>
-        </MDBRow>
-
-        <MDBBtn type='submit' block color='success'>
-          LOG IN
-        </MDBBtn>
-      </form>
-      <hr></hr>
-      <MDBBtn block color='success' tag='a' href='#'>
-          Cadastrar-se
-        </MDBBtn>
-
-    </div>
-  );
+					<MDBBtn type='submit' color='success' block>
+						Logar
+					</MDBBtn>
+				</form>
+				<hr></hr>
+				<MDBBtn block color='success' tag='a' href='#'>
+					Cadastrar-se
+				</MDBBtn>
+			</div>
+		</div>
+	);
 }
